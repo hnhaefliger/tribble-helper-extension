715b29981bf16ed3b3463813db088cb6b3582913
hnhaefliger <henry_haefliger@protonmail.com>
2021-11-11 14:21:28 -0500

    implement text body identification

--- src/js/listener.js
+++ src/js/listener.js
@@ -1,5 +1,15 @@
+function initListeners(element) {
+    if (getNodeText(element) === '') {
+        Array.from(element.children).forEach((child) => {
+            initListeners(child)
+        });
 
-Array.from(document.getElementsByTagName('p')).forEach((element) => {
+    } else {
+        addListeners(element);
+    }
+}
+
+function addListeners(element) {
     element.addEventListener('mouseover', (e) => {
         handleElementMouseOver(element, e);
     });
@@ -7,7 +17,7 @@ Array.from(document.getElementsByTagName('p')).forEach((element) => {
     element.addEventListener('mouseout', (e) => {
         handleElementMouseOut(element, e);
     });
-});
+}
 
 function handleElementMouseOver(element, event) {
     element.style.backgroundColor = 'green';
@@ -16,3 +26,26 @@ function handleElementMouseOver(element, event) {
 function handleElementMouseOut(element, event) {
     element.style.backgroundColor = '';
 }
+
+function getNodeText(element) {
+    element = element.cloneNode(true);
+
+    Array.from(element.children).forEach((child) => {
+        element.removeChild(child);
+    })
+
+    if (element.innerText) {
+        text = element.innerText;
+
+        text = text.replaceAll('\n', '');
+        text = text.replaceAll('\t', '');
+        text = text.replaceAll(new RegExp('( {2,})', 'g'), '');
+
+    } else {
+        text = '';
+    }
+
+    return text;
+}
+
+initListeners(document.body);
